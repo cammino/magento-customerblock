83c95b616f3878976e4977f0ee348e8415b488af
Mateus Rasteiro <mateusrasteiro10@gmail.com>
2024-10-15 15:54:16 -0300

    Fix customer attribute type

--- sql/cammino_customerblock_setup/install-1.0.0.php
+++ sql/cammino_customerblock_setup/install-1.0.0.php
@@ -1,13 +1,15 @@
 <?php
 
 $this->addAttribute('customer', 'is_customer_blocked', array(
-    'type'      => 'varchar',
+    'type'      => 'int',
     'label'     => 'Bloquear cliente',
-    'input'     => 'text',
+    'input'     => 'select',
+    'source'    => 'eav/entity_attribute_source_boolean', // Define como um campo Sim/Não
     'position'  => 120,
-    'required'  => false, //or true
+    'required'  => false,
     'is_system' => 0,
 ));
+
 $attribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'is_customer_blocked');
 $attribute->setData('used_in_forms', array(
     'adminhtml_customer',
